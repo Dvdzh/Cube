674053c6b1218190a627430412dc9044a9ddcbfc
David Zhu <david.zhu@telecom-sudparis.eu>
2023-01-26 00:34:20 +0100

    Working version

--- cube3d.py
+++ cube3d.py
@@ -1,25 +1,34 @@
 import numpy as np
-
-# -------------- constante --------------
 import pygame
+# -------------- constante --------------
 blue = (0, 0, 255)
 red = (255, 0, 0)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-x, y = 200, 200
+theta_x = 0.05
+theta_y = 0.05
+theta_z = 0.05
+
+weight, height = 1200, 600
+x_center, y_center = weight/2, height/2
 pas = 10
 
 # -------------- fonction --------------
+# créer les points
 
 
 def createPts():
     pts = {}
+    compt = 0
     for i in [-1, 1]:
         for j in [-1, 1]:
             for k in [-1, 1]:
-                pts["{}_{}_{}".format(i, j, k)] = np.array([i, j, k])
+                pts[compt] = np.array([i, j, k])
+                compt += 1
     return pts
+
+# calculer le nouveau vecteur de point
 
 
 def calculAngle(tx, ty, tz, vector):
@@ -38,18 +47,31 @@ def calculAngle(tx, ty, tz, vector):
     return (np.dot(np.dot(mx, np.dot(my, mz)), vector))
 
 
+def drawLine(pt1, pt2):
+    if pt1[2] < 0 or pt2[2] < 0:
+        pygame.draw.lines(dis, blue, False, [
+                          pt1[0:2]*100+[x_center, y_center], pt2[0:2]*100+[x_center, y_center]])
+    else:
+        pygame.draw.line(
+            dis, blue, pt1[0:2]*100+[x_center, y_center], pt2[0:2]*100+[x_center, y_center])
+    pygame.display.update()
+    return
+
+
 # --------------- script --------------
+# initialise les points
 pts = createPts()
-
-for i in pts:
-    print(pts[i])
+"""
+print(pts)
 raise ValueError()
-
+"""
+# crée la fenetre
 pygame.init()
-dis = pygame.display.set_mode((1200, 600))
+dis = pygame.display.set_mode((weight, height))
 pygame.display.set_caption('Cube 3D')
 pygame.display.update()
 
+# commence la fenetre
 i = 0
 game_over = False
 while not game_over:
@@ -58,25 +80,47 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
         if event.type == pygame.KEYDOWN:
+            dis.fill(white)
             if event.key == pygame.K_l:
                 game_over = True
-        """
             if event.key == pygame.K_z:
-                y -= pas
+                for i in range(8):
+                    pts[i] = calculAngle(
+                        0.05, 0, 0, pts[i])
             if event.key == pygame.K_q:
-                x -= pas
-            if event.key == pygame.K_s:
-                y += pas
+                for i in range(8):
+                    pts[i] = calculAngle(
+                        0, 0.05, 0, pts[i])
             if event.key == pygame.K_d:
-                x += pas
+                for i in range(8):
+                    pts[i] = calculAngle(
+                        0, 0, 0.05, pts[i])
+
         """
-        dis.fill(black)
         for j in pts:
             pygame.draw.circle(dis, red, pts[0], 10)
+        """
         # x_init, y_init, weight, height
-        pygame.draw.rect(dis, blue, [x, y, 10, 10])
+        dis.fill(black)
+        for i in range(8):
+            pts[i] = calculAngle(
+                theta_x, theta_y, theta_z, pts[i])
+        for i in range(8):
+            pygame.draw.circle(
+                dis, red, pts[i][0:2]*100+[x_center, y_center], 3, width=1)
+        drawLine(pts[1], pts[3])
+        drawLine(pts[7], pts[3])
+        drawLine(pts[7], pts[5])
+        drawLine(pts[1], pts[5])
+        drawLine(pts[0], pts[2])
+        drawLine(pts[6], pts[2])
+        drawLine(pts[6], pts[4])
+        drawLine(pts[4], pts[0])
+        drawLine(pts[0], pts[1])
+        drawLine(pts[2], pts[3])
+        drawLine(pts[4], pts[5])
+        drawLine(pts[6], pts[7])
+        pygame.draw.rect(dis, blue, [x_center, y_center, 10, 10])
         pygame.display.update()
-        i += 1
-        print(i)
 pygame.quit()
 quit()
